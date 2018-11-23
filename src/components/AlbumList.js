f30ec21967210d3e4ac5036e0f7378ecee8f5c0e
Zach Capshaw <zcapshaw@gmail.com>
2018-11-22 19:33:14 -0700

    Add Card and AlbumDetails components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail'
 
 export default class AlbumList extends Component {
 
@@ -16,7 +17,7 @@ export default class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album =>
-      <Text key={album.title}>{album.title}</Text>
+      <AlbumDetail key={album.title} album={album} />
     );
   }
 
